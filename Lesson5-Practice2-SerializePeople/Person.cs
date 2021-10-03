731696e9ec87e87e262aec3ec0fd489f6479ae48
Владислав Куманов <edu.kumanov@outlook.com>
2021-10-03 20:50:59 +0300

    Add project files.

---
+++ Lesson5-Practice2-SerializePeople/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Lesson5_Practice1_SerializePeople
+{
+    [Serializable]
+    public class Person
+    {
+        public string name;
+        public DateTime dateOfBirth;
+        [NonSerialized] public int age;
+
+        public Person(string _name, DateTime _dateOfBirth)
+        {
+            name = _name;
+            dateOfBirth = _dateOfBirth;
+            CalculateAge();
+        }
+
+        public Person()
+        {
+        }
+
+        public override string ToString()
+        {
+            return name + " was born on " + dateOfBirth.ToShortDateString() + " and is " + age.ToString() + " years old.";
+        }
+
+        private void CalculateAge()
+        {
+            age = DateTime.Now.Year - dateOfBirth.Year;
+
+            // If they haven't had their birthday this year, 
+            // subtract a year from their age
+            if (dateOfBirth.AddYears(DateTime.Now.Year - dateOfBirth.Year) > DateTime.Now)
+            {
+                age--;
+            }
+        }
+    }
+}
